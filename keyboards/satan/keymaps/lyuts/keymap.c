2cc2ecf3bd73d0dff2396b63c0524bc2a2ae5710
lyuts <dioxinu@gmail.com>
2018-07-29 17:59:37 -0700

    Replace KEYMAP_ANSI with LAYOUT_60_ansi.

--- keyboards/satan/keymaps/lyuts/keymap.c
+++ keyboards/satan/keymaps/lyuts/keymap.c
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |Ctrl |Gui  |Alt  |           Space            |fn2  |fn1  |fn2  |Ctrl  |
    * `-----------------------------------------------------------------------'
    */
-[_BL] = KEYMAP_ANSI(
+[_BL] = LAYOUT_60_ansi(
   F(ESC_TILD), KC_1,    KC_2,    KC_3,   KC_4,    KC_5,    KC_6,    KC_7,     KC_8,    KC_9,   KC_0,    KC_MINS, KC_EQL,  KC_BSPC, \
   F(TAB_FN2),  KC_Q,    KC_W,    KC_E,   KC_R,    KC_T,    KC_Y,    KC_U,     KC_I,    KC_O,   KC_P,    TD(O_BRCK), KC_RBRC, KC_BSLS, \
   MO(_L1),     KC_A,    KC_S,    KC_D,   KC_F,    KC_G,    KC_H,    KC_J,     KC_K,    KC_L,   KC_SCLN, KC_QUOT,          KC_ENT,  \
@@ -87,7 +87,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |    |    |    |                        |    |    |    |    |
    * `-----------------------------------------------------------'
    */
-[_L1] = KEYMAP_ANSI(
+[_L1] = LAYOUT_60_ansi(
   KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,    KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  \
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_HOME, KC_PGUP,  KC_UP,   KC_PGDN, KC_END,  KC_SLCK, KC_PAUS, KC_TRNS, \
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LEFT,  KC_DOWN, KC_RGHT, KC_TRNS, KC_TRNS,          KC_INS, \
@@ -125,7 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |    |    |    |                        |    |    |    |    |
    * `-----------------------------------------------------------'   
    */
-[_L2] = KEYMAP_ANSI(
+[_L2] = LAYOUT_60_ansi(
         /*
   #ifdef RGBLIGHT_ENABLE
   KC_GRV,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,   \
